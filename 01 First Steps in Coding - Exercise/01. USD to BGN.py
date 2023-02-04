b7f05181a043b135734cb8d62d05412292c965c6
Denislav Mladenov <124160873+denislav-mladenov@users.noreply.github.com>
2023-02-04 23:21:53 +0200

    Delete 01 First Steps in Coding - Exercise directory

--- 01 First Steps in Coding - Exercise/01. USD to BGN.py
+++
@@ -1,3 +0,0 @@
-usd = 1.79549
-bgn = float(input()) * usd
-print(bgn)
--- 01 First Steps in Coding - Exercise/09. Fish Tank.py
+++
@@ -1,8 +0,0 @@
-daljina = int(input())
-shirina = int(input())
-visochina = int(input())
-procent = float(input())
-obem_akva = daljina * shirina * visochina
-obem_litri = obem_akva / 1000
-total = obem_litri * (1 - (procent / 100))
-print(total)
