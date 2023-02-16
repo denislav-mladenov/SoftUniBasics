381653bbf6f4bab38d5f652085cd1fae629e0cb5
denislav-mladenov <denislav.mladenov.89@gmail.com>
2023-02-16 13:44:49 +0200

    Nested loop lab exercise 06

---
+++ 06 Nested Loops - Lab/06. Building.py
@@ -0,0 +1,19 @@
+floors = int(input())
+rooms = int(input())
+
+flat_number_counter = 0
+type_of_flat = ''
+
+for floor in range(floors, 0, - 1):
+    for a in range(rooms):
+        flat_number = floor * 10 + a
+
+        if floor == floors:
+            type_of_flat = f'L{flat_number}'
+        elif floor %2 == 0:
+            type_of_flat = f'O{flat_number}'
+        elif floor %2 != 0:
+            type_of_flat = f'A{flat_number}'
+        print(type_of_flat, end=' ')
+
+    print()
